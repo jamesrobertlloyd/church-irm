d368085bc3c484803dfaa09c61051f98d27c2415
James Robert Lloyd <jrl44@cam.ac.uk>
2013-03-12 10:56:11 +0000

    New post processing

--- source/postprocessing.py
+++ source/postprocessing.py
@@ -31,3 +31,26 @@ def print_stats(results_dir, fields=['ess', 'AUC']):
             print
             total_runtime += saved_results['runtime']
     print 'Total cpu time = %03.1f hours' % (total_runtime / 3600)
+    
+def latex_AUC_table(results_dir):
+    # Create a dictionary indexed by models and data sets - the produce latex table
+    models = sorted(os.listdir(results_dir))
+    data_sets = []
+    data = {}
+    for model in sorted(os.listdir(results_dir)):
+        data[model] = {}
+        local_data_sets = sorted(os.listdir(os.path.join(results_dir, model)))
+        data_sets = sorted(list(set(data_sets) | set(local_data_sets)))
+        for data_set in local_data_sets:
+            pickle_file = open(os.path.join(results_dir, model, data_set), 'rb')
+            saved_results = pickle.load(pickle_file)
+            pickle_file.close()
+            data[model][data_set] = saved_results['AUC']
+    # Dictionary is created, now produce LaTeX string
+    latex_string = 'Model & %s \\\\\n\\hline\n' % ' & '.join(data_sets)
+    print '          Model ',
+    print ' '.join(['%s' % data_set[:10].ljust(10) for data_set in data_sets])
+    for model in models:
+        print '%s  %s' % (model[:15].ljust(15), ' '.join(['     %0.3f' % data[model][data_set] for data_set in data_sets]))
+        latex_string = latex_string + model[:15] + ' & ' + ' & '.join(['%0.3f' % data[model][data_set] for data_set in data_sets]) + ' \\\\\n'
+    print latex_string
