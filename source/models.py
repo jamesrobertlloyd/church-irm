ef3791ad8d6472c9d8e8cf4657aac357dbc327ff
James Robert Lloyd <jrl44@cam.ac.uk>
2013-03-28 14:33:47 +0000

    Started writing schemey ILA

--- source/models.py
+++ source/models.py
@@ -248,6 +248,66 @@ class var_finite_LFRM(venture_model):
             an_id = self.RIPL.predict(parse('(p-friends %d %d)' % (i, j)))[0]
             missing_links.append(an_id)
         return (truth, missing_links)
+        
+class var_product_IRM(venture_model):
+    
+    def __init__(self, D=1, alpha=1, beta=1, symmetric=True):
+        self.D = D
+        self.alpha = alpha
+        self.beta = beta
+        self.symmetric = symmetric
+        
+    def description(self):
+        return 'Product_IRM_D=%s_alpha=%s_beta=%s_sym=%s' % (self.D, self.alpha, self.beta, self.symmetric)
+        
+    def create_RIPL(self):
+        # Create RIPL and clear any previous session
+        import venture_engine
+        self.RIPL = venture_engine
+        self.RIPL.clear()
+        
+        self.RIPL.assume('logistic', parse('(lambda (x) (/ 1 (+ 1 (power 2.71828 (- 0 x)))))')) #### TODO - replace me ASAP
+        self.RIPL.assume('list-product', parse('(lambda (lst) (if (> (length lst) 1) (* (first lst) (list-sum (rest lst))) (if (= (length lst) 1) (first lst) 1)))')) #### TODO - replace me if possible
+        self.RIPL.assume('map', parse('(lambda (f lst) (if (empty? lst) (list) (cons (f (first lst)) (map f (rest lst)))))')) #### TODO - replace me if possible
+        self.RIPL.assume('iota', parse('(lambda (n) (if (= n 0) (list) (cons n (iota (- n 1)))))')) #### TODO - replace me if possible
+        
+        self.RIPL.assume('D', parse('%s' % self.D))
+        self.RIPL.assume('alpha', parse('(mem (lambda (d) %s))' % self.alpha))
+        self.RIPL.assume('beta-a', parse('(mem (lambda (d) %s))' % self.beta))
+        self.RIPL.assume('beta-b', parse('(mem (lambda (d) %s))' % self.beta))
+        
+        self.RIPL.assume('cluster-crp', parse('(mem (lambda (d) (CRP/make (alpha d))))'))
+        
+        self.RIPL.assume('node->class', parse('(mem (lambda (node d) ((cluster-crp d))))'))
+        self.RIPL.assume('classes->parameters', parse('(mem (lambda (class1 class2 d) (beta (beta-a d) (beta-b d))))')) 
+        self.RIPL.assume('nodes->parameters', parse('(lambda (node1 node2 d) (classes->parameters (node->class node1 d) (node->class node2 d) d))')) 
+         
+        # Create relation probability function    
+        self.RIPL.assume('p-friends', parse('(lambda (node1 node2) (list-product (map (lambda (d) (nodes->parameters node1 node2 d)) (iota D))))'))
+        # Create relation evaluation function
+        self.RIPL.assume('friends', parse('(lambda (node1 node2) (bernoulli (p-friends node1 node2)))')) 
+       
+    def observe_data(self, observations):
+        '''Assumes triples of (i, j, v) for node i, node j and value v'''
+        for (i,j,v) in observations:
+            if v:
+                self.RIPL.observe(parse('(friends %d %d)' % (i, j)), 'true')
+                if self.symmetric:
+                    self.RIPL.observe(parse('(friends %d %d)' % (j, i)), 'true')
+            else:
+                self.RIPL.observe(parse('(friends %d %d)' % (i, j)), 'false')
+                if self.symmetric:
+                    self.RIPL.observe(parse('(friends %d %d)' % (j, i)), 'false')
+                    
+    def set_predictions(self, observations):
+        '''Assumes triples of (i, j, v) for node i, node j and value v'''
+        truth = []
+        missing_links = []
+        for (i,j,v) in observations:
+            truth.append(int(v))
+            an_id = self.RIPL.predict(parse('(p-friends %d %d)' % (i, j)))[0]
+            missing_links.append(an_id)
+        return (truth, missing_links)
                     
 class finite_2class_ILA(venture_model):
     
