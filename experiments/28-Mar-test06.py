d6acdbb217fde4a473a1f658429db046967c3f78
James Lloyd <james.robert.lloyd@gmail.com>
2013-03-28 16:06:18 +0000

    Update to experiment

--- experiments/28-Mar-test06.py
+++ experiments/28-Mar-test06.py
@@ -4,7 +4,7 @@
 'results_dir' : '../results/28-Mar-test06/',
 'data_dirs' : ['../data/50_nodes/'],
 'models' : [models.var_product_IRM]*1,
-'model_params' : [{'D' : '(uniform-discrete 1 4)'}],
+'model_params' : [{'D' : '(uniform-discrete 1 4)', 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}],
 'n_samples' : 100,
 'max_initial_run_time' : 30,
 'max_burn_time' : 60,
