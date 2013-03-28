2cd8006699d08ea83c4a7bc621ec32e2086f66cf
James Robert Lloyd <jrl44@cam.ac.uk>
2013-03-28 11:19:53 +0000

    Correct syntax

--- experiments/28-Mar-test02.py
+++ experiments/28-Mar-test02.py
@@ -4,7 +4,7 @@
 'results_dir' : '../results/28-Mar-test02/',
 'data_dirs' : ['../data/50_nodes/'],
 'models' : [models.finite_LFRM_scheme]*5,
-'model_params' : [{'D' : 02, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 04, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 06, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 08, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 10, 'alpha' : '(uniform-continuous 0.0001 3.0)'}],
+'model_params' : [{'D' : 2, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 4, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 6, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 8, 'alpha' : '(uniform-continuous 0.0001 3.0)'}, {'D' : 10, 'alpha' : '(uniform-continuous 0.0001 3.0)'}],
 'n_samples' : 100,
 'max_initial_run_time' : 30,
 'max_burn_time' : 60,
