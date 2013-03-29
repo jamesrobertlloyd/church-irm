d089c51e2618645ac3dd8f76ab011d886404da82
James Robert Lloyd <jrl44@cam.ac.uk>
2013-03-29 15:34:38 +0000

    Typo

--- experiments/29-Mar-test05.py
+++ experiments/29-Mar-test05.py
@@ -4,7 +4,7 @@
 'results_dir' : '../results/29-Mar-test05/',
 'data_dirs' : ['../data/lastfm_cs/'],
 'models' : [models.social_collab_IRM]*3,
-'model_params' : [{'D' : 1, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 3, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))', {'D' : 5, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}],
+'model_params' : [{'D' : 1, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 3, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 5, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}],
 'n_samples' : 100,
 'max_initial_run_time' : 300,
 'max_burn_time' : 600,
