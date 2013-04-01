5708266915f66023f6593b1c9fb2d09cc09109a6
James Robert Lloyd <jrl44@cam.ac.uk>
2013-04-01 10:26:08 +0100

    New post processing routine

--- source/postprocessing.py
+++ source/postprocessing.py
@@ -5,6 +5,17 @@ import pickle
 
 import models
 
+def print_all_AUCs(results_dir):
+    # Loop over model folders
+    for model in sorted(os.listdir(results_dir)):
+        for result in sorted(os.listdir(os.path.join(results_dir, model))):
+            pickle_file = open(os.path.join(results_dir, model, result), 'rb')
+            saved_results = pickle.load(pickle_file)
+            pickle_file.close()
+            print model, os.path.splitext(result)[0], 'ess = %04.f' % saved_results['ess'], 'AUC = %0.4f' % saved_results['AUC'], 'Memory = %04.f' % (saved_results['max_memory'] / (1024 * 1024))
+            for raw_result in saved_results['raw_results']:
+                print '  %0.4f' % raw_result['AUC']
+
 def print_basic_summary(results_dir):
     # Loop over model folders
     total_runtime = 0
