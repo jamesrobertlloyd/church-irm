55f2d2fd5bd9aca66ce4c5ce8a1ce73d374512fe
James Robert Lloyd <jrl44@cam.ac.uk>
2013-04-02 14:43:31 +0100

    Completing an experiment

---
+++ experiments/02-Apr-test02.py
@@ -0,0 +1,20 @@
+{
+'type' : 'cold_start',
+'verbose' : False,
+'results_dir' : '../results/02-Apr-test01/',
+'data_dirs' : ['../data/complement_another_lastfm/'],
+'models' : [models.social_collab_add_IRM]*4 + [models.social_collab_prod_IRM]*4,
+'model_params' : [{'D' : 1, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'sigma' : '(+ 0.01 (gamma 1 1))'}, {'D' : 3, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'sigma' : '(+ 0.01 (gamma 1 1))'}, {'D' : 5, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'sigma' : '(+ 0.01 (gamma 1 1))'}, {'D' : 7, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'sigma' : '(+ 0.01 (gamma 1 1))'}, {'D' : 1, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 3, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 5, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 7, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}],
+'n_samples' : 100,
+'max_initial_run_time' : 60,
+'max_burn_time' : 60,
+'max_sample_time' : 60,
+'intermediate_iter' : 1,
+'core_type' : 'f2',
+'cores_per_job' : 1,
+'n_restarts' : 100,
+'local_computation' : False,
+'use_realtime_cores' : True,
+'n_realtime_cores' : 100,
+'max_realtime_time' : 1
+}
