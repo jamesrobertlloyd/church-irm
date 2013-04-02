ae49d7fc45efd538898d6aeaf3acf1f50cbbe4c0
James Robert Lloyd <jrl44@cam.ac.uk>
2013-04-02 16:00:56 +0100

    New experiment for tomorrow

---
+++ experiments/03-Apr-test01.py
@@ -0,0 +1,18 @@
+{
+'type' : 'cold_start',
+'verbose' : False,
+'results_dir' : '../results/03-Apr-test01/',
+'data_dirs' : ['../data/all_lastfm_cs_folds/'],
+'models' : [models.social_collab_add_IRM]*2 + [models.social_collab_prod_IRM]*2,
+'model_params' : [{'D' : 1, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'sigma' : '(+ 0.01 (gamma 1 1))'}, {'D' : 7, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'sigma' : '(+ 0.01 (gamma 1 1))'}, {'D' : 1, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}, {'D' : 7, 'alpha' : '(uniform-continuous 0.0001 2.0)', 'beta' : '(+ 0.5 (gamma 1.0 1.0))'}],
+'n_samples' : 1000,
+'max_initial_run_time' : 60,
+'max_burn_time' : 600,
+'max_sample_time' : 600,
+'intermediate_iter' : 1,
+'core_type' : 'f2',
+'cores_per_job' : 1,
+'n_restarts' : 20,
+'local_computation' : False,
+'use_realtime_cores' : False,
+}
