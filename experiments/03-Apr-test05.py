e59b7e8d0e4d1d4e9d2f88fdd8202c93dee953f3
James Robert Lloyd <jrl44@cam.ac.uk>
2013-04-03 11:17:59 +0100

    A bunch of testing

---
+++ experiments/03-Apr-test05.py
@@ -0,0 +1,19 @@
+{
+'type' : 'cold_start',
+'verbose' : False,
+'results_dir' : '../results/03-Apr-test05/',
+'data_dirs' : ['../data/another_lastfm_cs_test/'],
+'models' : [models.social_collab_add_IRM]*1,
+'model_params' : [{'D' : 3, 'alpha' : '(uniform-continuous 0.0001 2.0)'}],
+'n_samples' : 10,
+'max_initial_run_time' : 30,
+'max_burn_time' : 30,
+'max_sample_time' : 30,
+'intermediate_iter' : 1,
+'core_type' : 'f2',
+'cores_per_job' : 1,
+'n_restarts' : 2,
+'local_computation' : False,
+'use_realtime_cores' : False,
+'thread_wait' : 5
+}
